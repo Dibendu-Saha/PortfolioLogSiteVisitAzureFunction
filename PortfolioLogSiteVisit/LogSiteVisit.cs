4f4446f9ff42509d818004c66b734ca0aa25d322
Dibendu Saha <dibendu4@gmail.com>
2022-12-17 19:48:08 +0530

    Social logo click notification + location null check

--- PortfolioLogSiteVisit/LogSiteVisit.cs
+++ PortfolioLogSiteVisit/LogSiteVisit.cs
@@ -27,26 +27,32 @@ namespace PortfolioLogSiteVisit
 
             string pw = Environment.GetEnvironmentVariable("App_Pw");
 
-            string tblLocationDetails
-                = "<hr><div>"
-                + "<p>Location details:</p>"
-                + "<table border=\"1\">"
-                + $"<tr> <td><strong>Country Code</strong></td> <td>{locData.Country_Code}</td> </tr>"
-                + $"<tr> <td><strong>Country</strong></td> <td>{locData.Country_Name}</td> </tr>"
-                + $"<tr> <td><strong>State</strong></td> <td>{locData.State}</td> </tr>"
-                + $"<tr> <td><strong>City</strong></td> <td>{locData.City}</td> </tr>"
-                + $"<tr> <td><strong>Postal</strong></td> <td>{locData.Postal}</td> </tr>"
-                + $"<tr> <td><strong>Latitude</strong></td> <td>{locData.Latitude}</td> </tr>"
-                + $"<tr> <td><strong>Longitude</strong></td> <td>{locData.Longitude}</td> </tr>"
-                + $"<tr> <td><strong>IPv4</strong></td> <td>{locData.IPv4}</td> </tr>"
-                + "</table></div><hr>";
+            string tblLocationDetails;
+            if (locData != null)
+                tblLocationDetails
+                    = "<hr><div>"
+                    + "<p>Location details:</p>"
+                    + "<table border=\"1\">"
+                    + $"<tr> <td><strong>Country Code</strong></td> <td>{locData.Country_Code}</td> </tr>"
+                    + $"<tr> <td><strong>Country</strong></td> <td>{locData.Country_Name}</td> </tr>"
+                    + $"<tr> <td><strong>State</strong></td> <td>{locData.State}</td> </tr>"
+                    + $"<tr> <td><strong>City</strong></td> <td>{locData.City}</td> </tr>"
+                    + $"<tr> <td><strong>Postal</strong></td> <td>{locData.Postal}</td> </tr>"
+                    + $"<tr> <td><strong>Latitude</strong></td> <td>{locData.Latitude}</td> </tr>"
+                    + $"<tr> <td><strong>Longitude</strong></td> <td>{locData.Longitude}</td> </tr>"
+                    + $"<tr> <td><strong>IPv4</strong></td> <td>{locData.IPv4}</td> </tr>"
+                    + "</table></div><hr>";
+            else
+                tblLocationDetails = "<hr><div><p>Location data not available.</p></div>";
 
             StringBuilder message = new StringBuilder();
             message.Append(
-                page == "Download CV"
-                    ? "<p>Someone just downloaded your CV.</p>"
-                    : $"<p>Someone just visited your website.</p>"
-                         + $"<p>Website section: <strong>{page}</strong></p>"
+                page.Contains("download")
+                    ? "<p>Visitor just downloaded your CV.</p>"
+                    : page.Contains("click")
+                        ? $"<p>Visitor just clicked on your social icon ({page.Split("_")[2]}).</p>"
+                        : "<p>Someone just visited your website.</p>"
+                             + $"<p>Website section: <strong>{page}</strong></p>"
                 );
 
 
@@ -64,9 +70,11 @@ namespace PortfolioLogSiteVisit
             emailMessage.Body = message.ToString();
 
             emailMessage.Subject =
-                page == "Download CV"
+                page.Contains("download")
                     ? "CV Download Notification - Dibendu Saha"
-                    : $"Site Visit Notification - Dibendu Saha ({page})";
+                    : page.Contains("click")
+                        ? $"Social Logo Click Notification - Dibendu Saha ({page.Split("_")[2]})"
+                        : $"Site Visit Notification - Dibendu Saha ({page})";
 
             smtpClient.Port = 587;
             smtpClient.Host = "smtp.gmail.com";
