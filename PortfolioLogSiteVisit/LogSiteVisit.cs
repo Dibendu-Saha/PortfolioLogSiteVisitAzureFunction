50ef13e0be0743f120d6482b39e161a35fdfbf67
Dibendu Saha <dibendu4@gmail.com>
2023-08-14 17:40:10 +0530

    Update code to handle exception and accept post request

--- PortfolioLogSiteVisit/LogSiteVisit.cs
+++ PortfolioLogSiteVisit/LogSiteVisit.cs
@@ -17,16 +17,54 @@ namespace PortfolioLogSiteVisit
     public static class LogSiteVisit
     {
         [FunctionName("LogSiteVisit")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            try
+            {
+                log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string page = req.Query["page"].ToString();
-            string loc_data_raw = req.Query["loc_data"].ToString();
-            LocationData locData = JsonConvert.DeserializeObject<LocationData>(loc_data_raw);
+                string page = req.Query["page"].ToString();
+                string loc_data_raw = await new StreamReader(req.Body).ReadToEndAsync();
+                var locData = JsonConvert.DeserializeObject<LocationData>(loc_data_raw);
 
-            string pw = Environment.GetEnvironmentVariable("App_Pw");
+                string pw = Environment.GetEnvironmentVariable("App_Pw");
+                string emailFrom = Environment.GetEnvironmentVariable("Email_From");
+                string emailTo = Environment.GetEnvironmentVariable("Email_To");
 
+                // Send email
+                MailMessage emailMessage = new MailMessage();
+                SmtpClient smtpClient = new SmtpClient();
+                emailMessage.From = new MailAddress(emailFrom);
+                emailMessage.To.Add(new MailAddress(emailTo));
+                emailMessage.IsBodyHtml = true;
+                emailMessage.Subject = GenerateEmailSubject(page, locData.Country_Name);
+                emailMessage.Body = GenerateEmailBody(page, locData);
+                smtpClient.Port = 587;
+                smtpClient.Host = "smtp.gmail.com";
+                smtpClient.EnableSsl = true;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(emailTo, pw);
+
+                smtpClient.Send(emailMessage);
+
+                return new OkResult();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        static string GenerateEmailSubject(string page, string country) =>
+            page.Contains("download")
+                    ? "CV Download Notification - Dibendu Saha"
+                    : page.Contains("click")
+                        ? $"Social Logo Click Notification - Dibendu Saha ({page.Split("_")[2]})"
+                        : $"Site Visit Notification - Dibendu Saha ({page}) - {country}";
+
+
+        static string GenerateEmailBody(string page, LocationData locData)
+        {
             string tblLocationDetails;
             if (locData != null)
                 tblLocationDetails
@@ -55,36 +93,7 @@ namespace PortfolioLogSiteVisit
                              + $"<p>Website section: <strong>{page}</strong></p>"
                 );
 
-
-            message.Append(tblLocationDetails);
-
-            string emailFrom = Environment.GetEnvironmentVariable("Email_From");
-            string emailTo = Environment.GetEnvironmentVariable("Email_To");
-
-            // Send email
-            MailMessage emailMessage = new MailMessage();
-            SmtpClient smtpClient = new SmtpClient();
-            emailMessage.From = new MailAddress(emailFrom);
-            emailMessage.To.Add(new MailAddress(emailTo));
-            emailMessage.IsBodyHtml = true;
-            emailMessage.Body = message.ToString();
-
-            emailMessage.Subject =
-                page.Contains("download")
-                    ? "CV Download Notification - Dibendu Saha"
-                    : page.Contains("click")
-                        ? $"Social Logo Click Notification - Dibendu Saha ({page.Split("_")[2]})"
-                        : $"Site Visit Notification - Dibendu Saha ({page})";
-
-            smtpClient.Port = 587;
-            smtpClient.Host = "smtp.gmail.com";
-            smtpClient.EnableSsl = true;
-            smtpClient.UseDefaultCredentials = false;
-            smtpClient.Credentials = new NetworkCredential(emailTo, pw);
-
-            smtpClient.Send(emailMessage);
-
-            return new OkResult();
+            return message.Append(tblLocationDetails).ToString();
         }
     }
 }
